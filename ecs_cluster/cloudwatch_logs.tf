3e6f1f14ef4540805cccfb34e2bfe200a7253cdb
Pim Snel <post@pimsnel.com>
2024-01-08 15:16:48 +0100

    disable logs because autocreated

--- ecs_cluster/cloudwatch_logs.tf
+++ ecs_cluster/cloudwatch_logs.tf
@@ -1,8 +1,9 @@
-resource "aws_cloudwatch_log_group" "cloudwatch_logs" {
-  name = "/aws/ecs/containerinsights/${aws_ecs_cluster.this.name}/performance"
-  kms_key_id = var.kms_key_arn
-
-  retention_in_days = 90
-
-  tags = var.tags
-}
+# NOTE this should be tested and seems to be created automatically
+#resource "aws_cloudwatch_log_group" "cloudwatch_logs" {
+#  name = "/aws/ecs/containerinsights/${aws_ecs_cluster.this.name}/performance"
+#  kms_key_id = var.kms_key_arn
+#
+#  retention_in_days = 90
+#
+#  tags = var.tags
+#}
