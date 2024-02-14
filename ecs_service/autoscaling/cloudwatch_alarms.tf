159fcd624fd2161071f37ec8bd2a74e9110682aa
jerpen80 <dra_penny@yahoo.com>
2024-02-14 08:33:53 +0100

    adjusted autoscale description to be more clear

--- ecs_service/autoscaling/cloudwatch_alarms.tf
+++ ecs_service/autoscaling/cloudwatch_alarms.tf
@@ -7,7 +7,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_high" {
   period              = 60
   statistic           = "Maximum"
   threshold           = var.threshold_cpu_high
-  alarm_description   = "autoscaling"
+  alarm_description   = "Autoscaling Alarm"
 
   dimensions = {
     ClusterName = var.ecs_cluster_name
@@ -26,7 +26,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_low" {
   period              = 60
   statistic           = "Maximum"
   threshold           = var.threshold_cpu_low
-  alarm_description   = "autoscaling"
+  alarm_description   = "Autoscaling Alarm"
 
   dimensions = {
     ClusterName = var.ecs_cluster_name
