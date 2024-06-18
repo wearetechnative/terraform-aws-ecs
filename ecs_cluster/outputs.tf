2e36ebff3aec047da11a33cb7549bbaca43d4907
Andrew Ng <andrew@technative.eu>
2024-06-18 20:11:29 +0200

    Added ecs cluster name output variable

--- ecs_cluster/outputs.tf
+++ ecs_cluster/outputs.tf
@@ -5,3 +5,7 @@ output "ecs_cluster_arn" {
 output "ec2_instance_role_ecs_policy" {
   value = jsondecode(data.aws_iam_policy_document.instance_ecs_policy.json)
 }
+
+output "ecs_cluster_name" {
+  value = aws_ecs_cluster.this.name
+}
